ae285805fde514270e58b7749fb6af4c88f75389
Ana Alejandra Castillejo Munoz <ana-cast@c3r1s3.42malaga.com>
2023-10-28 17:59:06 +0200

    Added bonus functions

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 20:57:21 by ana-cast          #+#    #+#             */
+/*   Updated: 2023/10/19 02:46:41 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+
+	current = *lst;
+	while (current)
+	{
+		*lst = current->next;
+		(*del)(current->content);
+		free(current);
+		current = *lst;
+	}
+}
---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 20:58:27 by ana-cast          #+#    #+#             */
+/*   Updated: 2023/10/19 02:49:25 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*start;
+	t_list	*node;
+
+	start = NULL;
+	while (lst)
+	{
+		node = ft_lstnew(0);
+		if (!node)
+		{
+			ft_lstclear(&start, del);
+			return (NULL);
+		}
+		node->content = f(lst->content);
+		ft_lstadd_back(&start, node);
+		lst = lst->next;
+	}
+	return (start);
+}
